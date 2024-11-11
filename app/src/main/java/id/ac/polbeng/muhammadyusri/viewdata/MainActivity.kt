5c344e0c2c44199208f2df40fb5ab6c99661d5d5
lenovo <yusrikualiitam@gmail.com>
2024-11-11 08:13:07 -0800

    viewdata.kt

---
+++ app/src/main/java/id/ac/polbeng/muhammadyusri/viewdata/MainActivity.kt
@@ -0,0 +1,28 @@
+package id.ac.polbeng.muhammadyusri.viewdata
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import id.ac.polbeng.muhammadyusri.viewdata.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnViewList.setOnClickListener{
+            startActivity(
+                Intent(applicationContext,
+                ListviewActivity::class.java)
+            )
+            binding.btnRecylerView.setOnClickListener{
+                startActivity(Intent(applicationContext,
+                    RecylerActivity::class.java))
+            }
+        }
+    }
+}
